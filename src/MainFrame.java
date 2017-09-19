4e4ee5dfa82410b7306fdb0e46bc503fb5c462bc
Prin0516 <105021046@live.asia.edu.tw>
2017-09-19 12:51:49 +0800

    second

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -7,6 +7,9 @@ import java.awt.event.WindowEvent;
 
 public class MainFrame extends Frame {
     private Button btnexit=new Button("exit");
+    private Button btnadd=new Button("add");
+    private Label lab=new Label("0");
+    private int c=0;
     public MainFrame(){
         init();
     }
@@ -14,8 +17,14 @@ public class MainFrame extends Frame {
         this.setLocation(10,10);
         this.setSize(500,500);
         this.setLayout(null);
-        btnexit.setBounds(200,200,100,100);
+        btnexit.setBounds(200,200,100,40);
+        btnadd.setBounds(200,250,100,40);
+        lab.setBounds(200,300,100,50);
+        btnexit.setBackground(new Color(200,200,100));
+        btnadd.setBackground(new Color(200,200,255));
         this.add(btnexit);
+        this.add(btnadd);
+        this.add(lab);
         this.addWindowListener(new WindowAdapter(){
             public void windowClosing(WindowEvent windowEvent ) {
                 System.exit(0);
@@ -25,6 +34,13 @@ public class MainFrame extends Frame {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 System.exit(0);
+            }
+        });
+        btnadd.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                c++;
+                lab.setText(Integer.toString(c));
             }
         });
 
